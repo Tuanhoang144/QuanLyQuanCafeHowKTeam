631bc1ff719762321695b14443b9fb2d63cd845e
ducnghia1205 <ducnghia1205@gmail.com>
2018-01-11 10:17:35 +0700

    Bài 6: Tạo chức năng đăng nhập Note: query cộng chuỗi

--- QuanLyQuanCafe/fLogin.cs
+++ QuanLyQuanCafe/fLogin.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuanLyQuanCafe.DAO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,10 +20,25 @@ namespace QuanLyQuanCafe
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            fTableManager f = new fTableManager();
-            this.Hide();
-            f.ShowDialog();
-            this.Show();
+            string userName = txtbUserName.Text;
+            string passWord = txtbPassWord.Text;
+
+            if (Login(userName,passWord))
+            {
+                fTableManager f = new fTableManager();
+                this.Hide();
+                f.ShowDialog();
+                this.Show();
+            }
+            else
+            {
+                MessageBox.Show("Sai tên tài khoản hoặc mật khẩu!");
+            }
+        }
+
+        bool Login(string username,string password)
+        {
+            return AccountDAO.Instance.Login(username, password);
         }
 
         private void btnExit_Click(object sender, EventArgs e)
